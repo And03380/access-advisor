ff8dd359d2922bb56ed907d6fdf0b92dde6c8160
And03380 <44380229+And03380@users.noreply.github.com>
2019-10-02 00:37:30 -0400

    Update get-access-advisor-for-each-role.py

--- get-access-advisor-for-each-role.py
+++ get-access-advisor-for-each-role.py
@@ -3,29 +3,15 @@ import boto3
 
 client = boto3.client('iam')
 
-response = client.generate_service_last_accessed_details(
-    Arn='arn:aws:iam::439463768147:role/EKS-Managment'
+f2 = ["arn:aws:iam::439463768147:role/flowlogsRole", "arn:aws:iam::439463768147:role/LambdaGuardrail-EC2"]
+
+fruits = ["apple", "banana", "cherry"]
+for roles in f2:
+    
+    response = client.generate_service_last_accessed_details(
+    Arn=roles
 )
 
 print(response)
 
-marker = None
 
-full_role_list = []
-
-while True:
-    paginator = client.get_paginator('generate_service_last_accessed_details')
-    response_iterator = paginator.paginate( 
-        PaginationConfig={
-            'StartingToken': marker})
-    for page in response_iterator:
-        print("Next Page : {} ".format(page['IsTruncated']))
-        u = page['Roles']
-        for user in u:
-            print(user)
-            
-    try:
-            marker = page['Marker']
-            print(marker)
-    except KeyError:
-            sys.exit()
