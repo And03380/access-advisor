c44d7338ce354d667580d7bdd3170c7795ee5e3d
And03380 <44380229+And03380@users.noreply.github.com>
2019-10-02 00:59:59 -0400

    Update and rename get-access-advisor-for-each-role.py to get-Jobid-for-each-role.py

---
+++ get-Jobid-for-each-role.py
@@ -0,0 +1,19 @@
+import sys
+import boto3
+
+client = boto3.client('iam')
+
+f2= open(Role_ARN.txt) 
+
+f2= open("ResourcesID.txt")
+Job_Ids_from_file = f2.read().split("\n")
+
+f3= open("JobId_list.txt","w+")
+
+for roles in Job_Ids_from_file:
+    
+    response = client.generate_service_last_accessed_details(Arn=roles)
+    f3.write(response['JobId'])
+
+
+
