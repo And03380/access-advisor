f62f3398d4ab740e704c25084272d6efb9979a1d
And03380 <44380229+And03380@users.noreply.github.com>
2019-10-02 01:46:35 -0400

    Update get-Jobid-for-each-role.py

--- get-Jobid-for-each-role.py
+++ get-Jobid-for-each-role.py
@@ -11,9 +11,10 @@ f3= open("JobId_list.txt","w+")
 
 for role in Job_Ids_from_file:
     string_role = str(role)
+    print("stringROLLLLL:    " + string_role)
     response = client.generate_service_last_accessed_details(
     Arn=string_role
 )
     
-    print(response['JobId'])
+    ##print(response['JobId'])
     f3.write(response['JobId'] + "\n")
