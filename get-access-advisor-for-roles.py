df16b284c757a3b061cb4ced428cd80b343a178a
And03380 <44380229+And03380@users.noreply.github.com>
2019-10-02 02:22:47 -0400

    Update get-access-advisor-for-roles.py

--- get-access-advisor-for-roles.py
+++ get-access-advisor-for-roles.py
@@ -1,1 +1,28 @@
+import sys
+import boto3
+import botocore
 
+client = boto3.client('iam')
+
+f2= open("JobId_list.txt")
+Job_Ids_from_file = f2.read().split("\n")
+f2.close()
+
+f3= open("out_puts.txt","w+")
+
+def HelperFun(x): 
+    y = str(x)
+    #print(y)
+    response = client.client.get_organizations_access_report(
+    JobId=y
+)
+    #print(response['JobId'])
+    f3.write(response + "\n")
+
+for role in Job_Ids_from_file:
+    HelperFun(role)
+    #response = client.generate_service_last_accessed_details(
+    #Arn="\"role\": \"" + role + "\""
+#)    
+    #print(response['JobId'])
+    #f3.write(response['JobId'] + "\n")
