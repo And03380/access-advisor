b07e2f8ac210b13d14c5a0da83460170ac5cd912
And03380 <44380229+And03380@users.noreply.github.com>
2019-10-01 23:11:58 -0400

    Update get-access-advisor.py

--- get-access-advisor.py
+++ get-access-advisor.py
@@ -5,5 +5,7 @@ iam = boto3.client('iam')
 role_list = iam.list_roles()
 
 paginator = iam.get_paginator('list_roles')
-for response in paginator.paginate():
-    print(response.arn)
+for response in paginator.paginate().arn:
+    print(response)
+
+    
