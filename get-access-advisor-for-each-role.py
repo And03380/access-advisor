3d668d610664ef1c1d1ce35e37a75f6212fbd527
And03380 <44380229+And03380@users.noreply.github.com>
2019-10-02 00:14:38 -0400

    Update get-access-advisor-for-each-role.py

--- get-access-advisor-for-each-role.py
+++ get-access-advisor-for-each-role.py
@@ -1,1 +1,10 @@
+import sys
+import boto3
 
+client = boto3.client('iam')
+
+response = client.generate_service_last_accessed_details(
+    Arn='arn:aws:iam::439463768147:role/EKS-Managment'
+)
+
+print(response)
