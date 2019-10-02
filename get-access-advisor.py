78ee87800f5f18a37510ef76d6ff4ea7269d9e29
And03380 <44380229+And03380@users.noreply.github.com>
2019-10-01 23:18:43 -0400

    Update get-access-advisor.py

--- get-access-advisor.py
+++ get-access-advisor.py
@@ -4,8 +4,19 @@ iam = boto3.client('iam')
 
 role_list = iam.list_roles()
 
-paginator = iam.get_paginator('list_roles')
-for response in paginator.paginate().arn:
-    print(response)
-
-    
+while True:
+    paginator = iam.get_paginator('list_roles')
+    response_iterator = paginator.paginate( 
+        PaginationConfig={
+            'PageSize': 10,
+            'StartingToken': marker})
+    for role in response_iterator:
+        print("Next Page : {} ".format(role['IsTruncated']))
+        u = role['Users']
+        for user in u:
+            print(user['UserName'])
+    try:
+        marker = role['Marker']
+        print(marker)
+    except KeyError:
+        sys.exit()
