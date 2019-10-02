6ccb37f3511b1e1d1ee166873c91a506c704ed89
And03380 <44380229+And03380@users.noreply.github.com>
2019-10-02 00:52:16 -0400

    Update get-access-advisor-for-each-role.py

--- get-access-advisor-for-each-role.py
+++ get-access-advisor-for-each-role.py
@@ -3,13 +3,14 @@ import boto3
 
 client = boto3.client('iam')
 
+f2= open("Results.csv","w+")
 f2 = ["arn:aws:iam::439463768147:role/flowlogsRole", "arn:aws:iam::439463768147:role/LambdaGuardrail-EC2", "arn:aws:iam::439463768147:role/RedlockReadOnlyRole",
      "arn:aws:iam::439463768147:role/S3ReadOnlyEC2Access"]
 
 for roles in f2:
     
     response = client.generate_service_last_accessed_details(Arn=roles)
-    print(response)
+    print(response['JobID'])
     print(1)
 
 
