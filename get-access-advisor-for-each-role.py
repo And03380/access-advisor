c3d659f5fc9e56419520e8223f1cca42f6912823
And03380 <44380229+And03380@users.noreply.github.com>
2019-10-02 00:25:20 -0400

    Update get-access-advisor-for-each-role.py

--- get-access-advisor-for-each-role.py
+++ get-access-advisor-for-each-role.py
@@ -18,7 +18,7 @@ while True:
     paginator = iam.get_paginator('generate_service_last_accessed_details')
     response_iterator = paginator.paginate( 
         PaginationConfig={
-            'JobId' : 2eb6c556-cdbc-acd4-2ad2-ffb57aadecae,
+            'JobId' : "2eb6c556-cdbc-acd4-2ad2-ffb57aadecae",
             'StartingToken': marker})
     for page in response_iterator:
         print("Next Page : {} ".format(page['IsTruncated']))
