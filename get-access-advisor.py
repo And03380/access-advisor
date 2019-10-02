044c65c78ff555594c1db4329627fb58712f25e2
And03380 <44380229+And03380@users.noreply.github.com>
2019-10-01 22:45:42 -0400

    Update get-access-advisor.py

--- get-access-advisor.py
+++ get-access-advisor.py
@@ -3,8 +3,6 @@ import boto3
 iam = boto3.resource('iam')
 role = iam.Role('name')
 
-list_roles(
-    PathPrefix='string',
-    Marker='string',
-    MaxItems=123
-)
+list_roles_resp = iam.list_roles()
+for role in list_roles_resp[]:
+    print(role)
