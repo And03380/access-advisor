65066d199fd07362b3291829b4ea3244f13d6522
And03380 <44380229+And03380@users.noreply.github.com>
2019-10-02 01:41:40 -0400

    Update get-Jobid-for-each-role.py

--- get-Jobid-for-each-role.py
+++ get-Jobid-for-each-role.py
@@ -11,6 +11,9 @@ f3= open("JobId_list.txt","w+")
 
 for role in Job_Ids_from_file:
     
-    response = client.generate_service_last_accessed_details(Arn='arn:aws:iam::439463768147:role/aws-service-role/es.amazonaws.com/AWSServiceRoleForAmazonElasticsearchService')
-    print(role)
+    response = client.generate_service_last_accessed_details(
+    Arn='arn:aws:iam::439463768147:role/aws-service-role/es.amazonaws.com/AWSServiceRoleForAmazonElasticsearchService'
+)
+    print("role print: " + role)
+    print("response print: " + response)
     f3.write(response['JobId'] + "\n")
