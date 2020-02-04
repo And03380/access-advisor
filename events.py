50adae7180e4898febd13a3635ef2dd26f4ad56d
And03380 <44380229+And03380@users.noreply.github.com>
2020-02-03 22:35:07 -0500

    Update events.py

--- events.py
+++ events.py
@@ -7,10 +7,14 @@ client_s3 = boto3.client('s3')
 client_firehouse = boto3.client('firehose')
 client_ec2 = boto3.client('ec2')
 client_sqs = boto3.client('sqs')
+client_kinesis = boto3.client('kinesis')
+client_events = boto3.client('events')
+client_sms = boto3.client('sms')
+
 
 def events():
     for x in range(100):
-        numberList = [1,2,3,4,5]
+        numberList = [1,2,3,4,5,6,7,8,9,10,11]
         selector = random.choice(numberList)
 
         if selector == 1:
@@ -31,6 +35,30 @@ def events():
 
         elif selector == 5:
             response = client_s3.list_buckets()
+            return response
+        
+        elif selector == 6:
+            response = client_events.describe_event_bus()
+            return response
+        
+        elif selector == 7:
+            response = client_ec2.describe_ipv6_pools()
+            return response
+        
+        elif selector == 8:
+            response = client_events.describe_local_gateway_virtual_interfaces()
+            return response
+        
+        elif selector == 9:
+            response = client_kinesis.list_streams()
+            return response
+        
+        elif selector == 10:
+            response = client_sms.list_apps()
+            return response
+        
+        elif selector == 11:
+            response = client_ec2.describe_tags()
             return response
 
         else:
